ddd58e55940047abbd4cabf77167ef8e5b7d9c8b
CrazyGladiator <88976120+CrazyGladiator@users.noreply.github.com>
2022-02-19 01:16:10 +0000

    Update status_config.js

--- config/status_config.js
+++ config/status_config.js
@@ -1,7 +1,7 @@
 module.exports = {
  // Stauts config
  options: {
-  type: "dynamic", // dynamic | static
+  type: "static", // dynamic | static
  },
  static: {
   // Only if options.stype = static
