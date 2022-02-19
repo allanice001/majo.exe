f87f0a02d21dafa4c582f55674c0419a85634461
CrazyGladiator <88976120+CrazyGladiator@users.noreply.github.com>
2022-02-19 01:14:45 +0000

    Update categories_config.js

--- config/categories_config.js
+++ config/categories_config.js
@@ -8,7 +8,6 @@ module.exports = {
  utility: "Just useful commands that will always help you ._.",
  image: "Photo manipulation, meme generator, etc ^^",
  owner: "There is no proof that there are owner-only commands here ;-;",
- nsfw: "Some hot pictures for long evenings. For adults only!",
  giveaway: "Giveaways, drops and much more $.$",
  setup: "Here you can configure Majo.exe however you want!",
  social: "Create your profile, add social media, nicknames and share it!",
